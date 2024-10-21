21c53bbd4b669d7733fe7a3e69873c9c4431190d
Univer's <cynwal.bureau@icloud.com>
2024-10-21 20:00:45 +0200

    [ADD] - add folder [New] to ve_civil in [Vehicle]

---
+++ resources/[Vehicle]/ve_civil/fxmanifest.lua
@@ -0,0 +1,204 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+	'data/vehicles.meta',
+	'data/carvariations.meta',
+	'data/carcols.meta',
+	'data/handling.meta',
+	'data/vehiclelayouts.meta',
+	'data/vehiclelayouts_patriot.meta',
+	'data/vehiclelayouts_rumpoesc.meta',
+	'data/vehiclelayouts_tasty.meta',
+	'data/vehiclelayouts_taxi.meta',
+	'data/vehiclelayouts_weazel.meta',
+	--TAXI
+	'audio/polcarasound_game.dat151.rel',
+	'audio/umkscout_game.dat151.rel',
+	--WEAZEL
+	'audio/news_game.dat151.rel',
+	--SUPERGTS
+	'audio/supergts_game.dat151.rel',
+	'audio/supergts_sounds.dat54.rel',
+	'audio/sfx/dlc_supergts/supergts.awc',
+	'audio/sfx/dlc_supergts/supergts_npc.awc',
+	--STANIER 2
+	'audio/stanier2_game.dat151.rel',
+	--CADDYXL
+	'audio/caddyxl_game.dat151.rel',
+	--ARIANT
+	'audio/ariant_game.dat151.rel',
+	'audio/ariant_sounds.dat54.rel',
+	'audio/sfx/dlc_ariant/ariant.awc',
+	'audio/sfx/dlc_ariant/ariant_npc.awc',
+	--BANSHEE
+	'audio/bansheepo_game.dat151.rel',
+	'audio/bansheepo_sounds.dat54.rel',
+	'audio/sfx/dlc_bansheepo/bansheepo.awc',
+	'audio/sfx/dlc_bansheepo/bansheepo_npc.awc',
+	--CYPHERCT
+	'audio/cypherct_game.dat151.rel',
+	'audio/cypherct_sounds.dat54.rel',
+	'audio/sfx/dlc_cypherct/cypherct.awc',
+	'audio/sfx/dlc_cypherct/cypherct_npc.awc',
+	--SCHARMANN
+	'audio/scharmann_amp.dat10.rel',
+	'audio/scharmann_game.dat151.rel',
+	'audio/scharmannv8_sounds.dat54.rel',
+	'audio/scharmann_sounds.dat54.rel',
+	'audio/scharmannv8_game.dat151.rel',
+	'audio/sfx/dlc_scharmann/scharmann.awc',
+	'audio/sfx/dlc_scharmann/scharmann_npc.awc',
+	'audio/sfx/dlc_scharmannv8/scharmannv8.awc',
+	'audio/sfx/dlc_scharmannv8/scharmannv8_npc.awc',
+	--PATRIOT
+	'audio/patriotclassic_game.dat151.rel',
+	--VORSTAND
+	'audio/vorstand_game.dat151.rel',
+	'audio/vorstand_sounds.dat54.rel',
+	'audio/sfx/dlc_vorstand/vorstand.awc',
+	'audio/sfx/dlc_vorstand/vorstand_npc.awc',
+	--NSPEEDO
+	'audio/nspeedo_game.dat151.rel',
+	'audio/nspeedo_sounds.dat54.rel',
+	'audio/sfx/dlc_nspeedo/nspeedo.awc',
+	'audio/sfx/dlc_nspeedo/nspeedo_npc.awc',
+	--ARGENTO
+	'audio/argento_game.dat151.rel',
+	'audio/argento_sounds.dat54.rel',
+	'audio/sfx/dlc_argento/argento.awc',
+	'audio/sfx/dlc_argento/argento_npc.awc',
+	--SUNRISE
+	'audio/sunrise_game.dat151.rel',
+	'audio/sunrise_sounds.dat54.rel',
+	'audio/sfx/dlc_sunrise/sunrise.awc',
+	'audio/sfx/dlc_sunrise/sunrise_npc.awc',
+	--NEBULAW
+	'audio/nebulaw_game.dat151.rel',
+	--RUMPOESC
+	'audio/rumpoesc_game.dat151.rel',
+	--TORRENCE
+	'audio/torrence_game.dat151.rel',
+	--BUFFALOH
+	'audio/buffaloh_game.dat151.rel',
+	'audio/buffaloh_sounds.dat54.rel',
+	'audio/sfx/dlc_gresleyh/gresleyh.awc',
+	'audio/sfx/dlc_gresleyh/gresleyh_npc.awc',
+	--GRESLEYH
+	'audio/gresleyh_amp.dat10.rel',
+	'audio/gresleyh_game.dat151.rel',
+	'audio/gresleyh_sounds.dat54.rel',
+	'audio/sfx/dlc_gresleyh/gresleyh.awc',
+	'audio/sfx/dlc_gresleyh/gresleyh_npc.awc',
+	--TAMPAR
+	'audio/tampar_game.dat151.rel',
+	'audio/tampar_sounds.dat54.rel',
+	'audio/sfx/dlc_tampar/tampar.awc',
+	'audio/sfx/dlc_tampar/tampar_npc.awc',
+	--EXECUTIONER
+	'audio/executioner_game.dat151.rel',
+	'audio/executioner_sounds.dat54.rel',
+	--SENTINELSG4
+	'audio/sentinelsg4_game.dat151.rel',
+	'audio/sentinelsg4_sounds.dat54.rel',
+	'audio/sfx/dlc_sentinelsg4/sentinelsg4.awc',
+	'audio/sfx/dlc_sentinelsg4/sentinelsg4_npc.awc',
+	--MRTASTY
+	'audio/mrtasty_game.dat151.rel',
+	'audio/mrtasty_sounds.dat54.rel',
+	--'audio/sfx/dlc_mrtasty/ambient_mrtasty.awc',
+	'audio/sfx/dlc_mrtasty/mrtasty.awc',
+}
+
+----------------------------DATA FILE----------------------------------
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_patriot.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_rumpoesc.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_tasty.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_taxi.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_weazel.meta'
+--WEAZEL
+data_file 'AUDIO_GAMEDATA' 'audio/news_game.dat'
+--TAXI
+data_file 'AUDIO_GAMEDATA' 'audio/polcarasound_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/umkscout_game.dat'
+--SUPERGTS
+data_file 'AUDIO_GAMEDATA' 'audio/supergts_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/supergts_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_supergts'
+--STANIER 2
+data_file 'AUDIO_GAMEDATA' 'audio/stanier2_game.dat'
+--CADDYXL
+data_file 'AUDIO_GAMEDATA' 'audio/caddyxl_game.dat'
+--ARIANT
+data_file 'AUDIO_GAMEDATA' 'audio/ariant_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/ariant_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_ariant'
+--BANSHEE
+data_file 'AUDIO_GAMEDATA' 'audio/bansheepo_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/bansheepo_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_bansheepo'
+--CYPHERCT
+data_file 'AUDIO_GAMEDATA' 'audio/cypherct_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/cypherct_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_cypherct'
+--SCHARMANN
+data_file 'AUDIO_SYNTHDATA' 'audio/scharmann_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/scharmann_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/scharmann_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/scharmannv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/scharmannv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_scharmann'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_scharmannv8'
+--PATRIOT
+data_file 'AUDIO_GAMEDATA' 'audio/patriotclassic_game.dat'
+--VORSTAND
+data_file 'AUDIO_GAMEDATA' 'audio/vorstand_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/vorstand_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_vorstand'
+--NSPEEDO
+data_file 'AUDIO_GAMEDATA' 'audio/nspeedo_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/nspeedo_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_nspeedo'
+--ARGENTO
+data_file 'AUDIO_GAMEDATA' 'audio/argento_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/argento_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_argento'
+--SUNRISE
+data_file 'AUDIO_GAMEDATA' 'audio/sunrise_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/sunrise_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_sunrise'
+--NEBULAW
+data_file 'AUDIO_GAMEDATA' 'audio/nebulaw_game.dat'
+--RUMPOESC
+data_file 'AUDIO_GAMEDATA' 'audio/rumpoesc_game.dat'
+--TORRENCE
+data_file 'AUDIO_GAMEDATA' 'audio/torrence_game.dat'
+--BUFFALOH
+data_file 'AUDIO_GAMEDATA' 'audio/buffaloh_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/buffaloh_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_gresleyh'
+--GRESLEYH
+data_file 'AUDIO_SYNTHDATA' 'audio/gresleyh_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/gresleyh_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/gresleyh_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_gresleyh'
+--TEMPAR
+data_file 'AUDIO_GAMEDATA' 'audio/tampar_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/tampar_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_tampar'
+--EXECUTIONER
+data_file 'AUDIO_GAMEDATA' 'audio/executioner_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/executioner_sounds.dat'
+--SENTINELSG4
+data_file 'AUDIO_GAMEDATA' 'audio/sentinelsg4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/sentinelsg4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_sentinelsg4'
+--MRTASTY
+data_file 'AUDIO_GAMEDATA' 'audio/mrtasty_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/mrtasty_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_mrtasty'
