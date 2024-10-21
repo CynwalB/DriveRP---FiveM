30681d7424c19394ca4adb5447375b1591c36b88
Univer's <cynwal.bureau@icloud.com>
2024-10-21 19:32:09 +0200

    [ADD] - add folder [Smartphone]

---
+++ resources/[Smartphone]/qs-videos/config/config.js
@@ -0,0 +1,3 @@
+Config = []
+
+Config.Webhook = 'https://discord.com/api/webhooks/1186782031284281344/v6Zd8JHSnvBww4wSLnOG8i7xqCR4iYOxnsxjyP8TTOaFzZYn5tXW9tpKofkkWNSoTQCQ' // Set your own discord Webhook here.
